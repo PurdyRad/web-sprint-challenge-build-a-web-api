a7111251e3ef8c5a75d1b381949e951a5549b576
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-09 14:18:07 -0400

    insert mario waho here because I finished mvp

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,4 +1,3 @@
-// Write your "projects" router here!
 const express = require('express');
 const Project = require('./projects-model');
 const { validateProjectId, validateProject } = require('../middleware/middleware');
@@ -8,30 +7,45 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     Project.get()
     .then(projects => {
-        res.status(200).json(projects)
+        res.status(200).json(projects);
     })
-    .catch(next)
+    .catch(next);
 });
 
-router.get('/:id', validateProjectId, (req, res, next) => {
-    res.status(200).json(req.project)
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.status(200).json(req.project);
 });
+
+
+router.get('/:id/actions', validateProjectId, (req, res, next) => {
+    // res.status(200).json(req.project);
+    Project.getProjectActions(req.params.id)
+    .then(projectsActions => {
+        console.log('projectsActions', projectsActions);
+        res.status(200).json(projectsActions);
+    })
+    .catch(next);
+});
+
 
 router.post('/', validateProject, (req, res, next) => {
     Project.insert(req.body)
-    .then(dasDaNewProject => {
-        res.status(201).json(dasDaNewProject);
+    .then(daNewProject => {
+        res.status(201).json(daNewProject);
     })
     .catch(next);
 });
 
+
 router.put('/:id', validateProject, validateProjectId, (req, res, next) => {
     Project.update(req.params.id, req.body)
-    .then(dasIt => {
-        res.status(201).json(dasIt);
+    .then(daUpdatedProject => {
+        res.status(201).json(daUpdatedProject);
     })
     .catch(next);
 });
+
 
 router.delete('/:id', validateProjectId, (req, res, next) => {
     Project.remove(req.params.id)
@@ -39,8 +53,8 @@ router.delete('/:id', validateProjectId, (req, res, next) => {
         res.status(204).json(numberOfDeletedProjects);
     })
     .catch(next);
-
 });
+
 
 router.use ((err, req,res,next) => { // eslint-disable-line
     res.status(err.status || 500).json({
@@ -49,5 +63,6 @@ router.use ((err, req,res,next) => { // eslint-disable-line
       customMessage: "Ya won't find what you're lookin for here."
     }); 
   });
+
 
 module.exports = router;
